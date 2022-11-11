5d1d90740a9e5cc84bb7899827b81dfd4519380c
Noah <hankanz@outlook.com>
2022-11-11 17:51:29 -0500

    formPage request handler

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+import csv
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,12 +7,25 @@ def loginPage():
     return render_template('./static/login.html')
 @app.route('/makeAccount',methods=["POST"])
 def makeAccount():
-    user = request.form["username"]
-    pw = request.form["pw"]
+    email = request.form["email"]
+    password = request.form["password"]
 
 
     return render_template("./static/form.html")
-@app.route('/form',methods=["POST"]):
+@app.route('/form',methods=["POST"])
 def formPage():
-    user = request.form["username"]
-    pw = request.form["pw"]
+    usertype = request.form["usertype"]
+    email = request.form["email"]
+    password = request.form["password"]
+    if usertype == '':
+        with open('userCredentials.csv') as credentials:
+            reader = csv.reader(credentials,delimiter=',')
+            for row in reader:
+                if email == row[0]:
+                    if password == row[1]:
+                        return render_template('./static/form.html',email = email, password = password)
+                    else:
+                        break
+            return render_template('/static/login.html',invalidLogin = True)
+    else:
+        pass
