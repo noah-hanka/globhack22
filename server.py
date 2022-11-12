ee6e8b8a50958cf20164a03a985f5dda791613e4
Rohan Rajagopalan <rohan.rajagopalan274@gmail.com>
2022-11-11 22:44:16 -0500

    Update server.py

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ def makeAccount():
     with open('./db/userCredentials.csv', 'a+') as cred:
         csv_reader = csv.reader(cred, delimiter=",")
         for row in csv_reader:
-            if email == row[0]:
+            if email in row[0]:
                 # redirect to invalid login
                 return render_template("createAccount.html",invalidLogin=True)
         csv_writer = DictWriter(cred, fieldnames=fields)
