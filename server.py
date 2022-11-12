6e89332a1dd42e6d812e8603aeaa5d567fbba64f
J9Jones9 <j9jones910@gmail.com>
2022-11-12 00:46:15 -0500

    updating server

--- server.py
+++ server.py
@@ -1,33 +1,43 @@
 from flask import Flask, render_template, redirect, url_for, request
 import csv
 from csv import DictWriter
+import matplotlib.pyplot as plt
+
 app = Flask(__name__)
 
 # langing page
+
+
 @app.route('/')
 def landingPage():
     return render_template('landingpage.html')
 
 # general user login routing
+
+
 @app.route('/userlogin')
 def userLogin():
     return render_template('userlogin.html')
-@app.route('/form',methods=["POST"])
+
+
+@app.route('/form', methods=["POST"])
 def form():
     email = request.form["email"]
     password = request.form["password"]
     with open('./db/userCredentials.csv') as credentials:
-        reader = csv.reader(credentials,delimiter=',')
+        reader = csv.reader(credentials, delimiter=',')
         for row in reader:
             if email == row[0]:
                 if password == row[1]:
-                    return render_template('form.html',email = email, password = password)
+                    return render_template('form.html', email=email, password=password)
                 else:
                     break
-    return render_template('userlogin.html',invalidLogin = True)
+    return render_template('userlogin.html', invalidLogin=True)
+
+
 @app.route('/submitForm', methods=["POST"])
 def submitForm():
-    
+
     email = request.form["email"]
     password = request.form["password"]
 
@@ -41,8 +51,10 @@ def submitForm():
     shelter = request.form["group4"]
     tp = request.form["group5"]
 
-    fields = ["email", "password", "street_address", "city", "state", "zipCode", "water", "food", "electricity", "shelter", "tp"]
-    row_app = {"email":email, "password":password, "street_address":address, "city":city, "state":state, "zipCode":zipCode, "water":water, "food":food, "electricity":elec, "shelter":shelter, "tp":tp}
+    fields = ["email", "password", "street_address", "city", "state",
+              "zipCode", "water", "food", "electricity", "shelter", "tp"]
+    row_app = {"email": email, "password": password, "street_address": address, "city": city, "state": state,
+               "zipCode": zipCode, "water": water, "food": food, "electricity": elec, "shelter": shelter, "tp": tp}
 
     with open('./db/formEntry.csv', 'r+') as form:
         csv_reader = csv.reader(form, delimiter=",")
@@ -54,35 +66,45 @@ def submitForm():
         csv_writer.writerow(row_app)
 
 # administrator login routing
+
+
 @app.route('/adminlogin')
 def adminLogin():
     return render_template('adminlogin.html')
 
-@app.route('/admin',methods=["POST"])
+
+@app.route('/admin', methods=["POST"])
 def admin():
     email = request.form["email"]
     password = request.form["password"]
     with open('./db/adminCredentials.csv') as credentials:
-        reader = csv.reader(credentials,delimiter=',')
+        reader = csv.reader(credentials, delimiter=',')
         for row in reader:
             if email == row[0] and password == row[1]:
                 people = []
-                fields = ["email","password","street_address","city","state","zipCode","water","food","electricity","shelter","tp"]
+                fields = ["email", "password", "street_address", "city", "state",
+                          "zipCode", "water", "food", "electricity", "shelter", "tp"]
                 with open('./db/formEntry.csv') as peopleFile:
-                    reader = csv.reader(peopleFile,delimiter=',')
+                    reader = csv.reader(peopleFile, delimiter=',')
                     for row in reader:
-                        newDic = {fields[i]:row[i] for i in range(len(fields))}
+                        newDic = {fields[i]: row[i]
+                                  for i in range(len(fields))}
                         people.append(newDic)
                 n = len(people)
-                return render_template('admin.html',email = email, password = password, people = people, count = n)
-    return render_template('adminlogin.html',invalidLogin = True)
+
+                myCounts = getCounts(people)
+                makeGraph(myCounts)
+                return render_template('admin.html', email=email, password=password, people=people, count=n)
+    return render_template('adminlogin.html', invalidLogin=True)
 
 
 # creating account routing
 @app.route('/createAccount')
 def createAccount():
     return render_template('createAccount.html')
-@app.route('/makeAccount',methods=["POST"])
+
+
+@app.route('/makeAccount', methods=["POST"])
 def makeAccount():
     email = request.form["email"]
     pw = request.form["password"]
@@ -92,15 +114,50 @@ def makeAccount():
 
     fields = ['email', 'password', 'birthdate', 'lic_no', 'ssn']
     # row to append to csv
-    row_app = {'email':email, 'password':pw, 'birthdate':bd, 'lic_no':licno, 'ssn':ssn} 
+    row_app = {'email': email, 'password': pw,
+               'birthdate': bd, 'lic_no': licno, 'ssn': ssn}
 
     with open('./db/userCredentials.csv', 'r+') as cred:
         csv_reader = csv.reader(cred, delimiter=",")
         for row in csv_reader:
             if email == row[0]:
                 # redirect to invalid login
-                return render_template("createAccount.html",invalidLogin=True)
+                return render_template("createAccount.html", invalidLogin=True)
         csv_writer = DictWriter(cred, fieldnames=fields)
         csv_writer.writerow(row_app)
-        return render_template("form.html",email=email,password=pw)
+        return render_template("form.html", email=email, password=pw)
 
+
+plt.style.use('ggplot')
+
+
+def getCounts(somePeople):
+    counts = [0, 0, 0, 0, 0]
+    for person in somePeople:
+        if person['water'] == 'yes':
+            counts[0] += 1
+        if person['food'] == 'yes':
+            counts[1] += 1
+        if person['electricity'] == 'yes':
+            counts[2] += 1
+        if person['tp'] == 'yes':
+            counts[3] += 1
+        if person['shelter'] == 'yes':
+            counts[4] += 1
+
+    return counts
+
+
+def makeGraph(actualCount):
+
+    topics = ['Water', 'Food', 'Electricity', 'Toiletries', 'Shelter']
+    x_pos = [i for i, _ in enumerate(topics)]
+
+    plt.bar(x_pos, actualCount, color='blue')
+    plt.xlabel("Citizen Needs")
+    plt.ylabel("Total Citizens in Need")
+    plt.title("What Our Citizens Need")
+    plt.xticks(x_pos, topics)
+
+    plt.savefig("./static/output.jpg")
+    return
